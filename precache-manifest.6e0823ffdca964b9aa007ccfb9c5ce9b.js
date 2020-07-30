0a4a6c22114a47ac916044c4ddb003727a876e2b
Vikas Gupta <vikas.gupti047@gmail.com>
2020-07-30 14:08:45 +0530

    Updates

---
+++ precache-manifest.6e0823ffdca964b9aa007ccfb9c5ce9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2963c6242a7221668a6805cb968ea1ec",
+    "url": "/MemeGen/index.html"
+  },
+  {
+    "revision": "5a89e9f7bcb5fe787063",
+    "url": "/MemeGen/static/css/main.9a8f1bfa.chunk.css"
+  },
+  {
+    "revision": "6b1682d8a58b4dd2c5d9",
+    "url": "/MemeGen/static/js/2.065a6228.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MemeGen/static/js/2.065a6228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a89e9f7bcb5fe787063",
+    "url": "/MemeGen/static/js/main.190334b5.chunk.js"
+  },
+  {
+    "revision": "13e6088a8edfa6360097",
+    "url": "/MemeGen/static/js/runtime-main.71f9a612.js"
+  }
+]);
